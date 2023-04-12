38de6b2eb03caedb5d996e0a306b57067665f169
Cisco <ericcorona3@gmail.com>
2023-04-12 00:02:23 -0700

    pick seats
    tests picks random seats and the cost is correct

--- cypress/e2e/pick-seats.cy.js
+++ cypress/e2e/pick-seats.cy.js
@@ -1,18 +1,17 @@
 describe('pick seats', () => {
     it('picks multiple seats', () => {
       cy.visit('/');
+      let numSeats;
       cy.get('.row > .seat:not(.occupied)')
         .then(($seats) => {
-          let numSeats = Cypress._.random(2, 5); // Select between 2-5 seats
+          numSeats = Cypress._.random(2, 5); // Select between 2-5 seats
           const selectedSeats = Cypress._.sampleSize($seats, numSeats); // Select random seats
           selectedSeats.forEach(($seat) => {
             cy.wrap($seat).click(); // Click on the seat
           });
-        })
-        .then(() => {
-            cy.get('#count')
-              .invoke('val')
-              .should('have.value', numSeats); // Verify that the count matches the number of selected seats
-          });
+          cy.get('#count')
+            .invoke('text')
+            .should('equal', numSeats.toString()); // Verify that the count matches the number of selected seats
         });
-  
+    });
+  });
